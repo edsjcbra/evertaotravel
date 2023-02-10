dbf537084596bc0cdaa4fabae2405a5d83ac34a8
Ederson Silva <edsjcbra@gmail.com>
2023-02-10 15:10:41 +0000

    Added Ticket Class

---
+++ EvertaoWorldTravel/Ticket.cs
@@ -0,0 +1,23 @@
+﻿namespace EvertaoWorldTravel
+{
+    internal class Ticket
+    {
+        public Ticket(string origin, string destination, float price, DateTime travelDate)
+        {
+            Origin = origin;
+            Destination = destination;
+            Price = price;
+            TravelDate = travelDate;
+        }
+        public int Id { get; set; }
+        public string Origin { get; private set; }
+        public string Destination { get; private set; }
+        public float Price { get; private set; }
+        public DateTime TravelDate { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Num - Origem - Destino - Preco - Data\n {Id}      {Origin}       {Destination}     {Price}   {TravelDate}";
+        }
+    }
+}
